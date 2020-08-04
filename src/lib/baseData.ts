32be3377c8081d40cb1649c4bf517652753c3ee7
Jan Lehmkuhl <DaClan008@users.noreply.github.com>
2020-08-04 11:09:31 +0200

    ➕ sizeChange func & ➖ altered property (redunant)

--- src/lib/baseData.ts
+++ src/lib/baseData.ts
@@ -3,7 +3,7 @@ import * as Events from './events';
 
 export abstract class BaseData extends EventEmitter {
 	/* istanbul ignore next: default keys should not be relevant in table */
-	constructor(readonly key = '', readonly row = -1) {
+	constructor(readonly key = '', readonly row = -1, readonly maxsize = -1) {
 		super();
 	}
 
@@ -17,26 +17,23 @@ export abstract class BaseData extends EventEmitter {
 
 	abstract get lines(): string[];
 
-	set altered(val: boolean) {
-		/* istanbul ignore if: false is never called in table - safety */
-		if (!val) return;
-
-		// build lines
-		/* istanbul ignore else: no else */
-		if (this.buildLines()) this.emit(Events.EventDataChanged, this);
-	}
-
 	/** Get or set the size of the column */
 	set size(val: number) {
 		/* istanbul ignore else: no else */
-		if (val !== this.sze) {
-			this.sze = Math.max(-1, val);
-			this.altered = true;
+		if (val === this.sze) return;
+		if (val > this.maxsize && this.maxsize > -1) this.sze = this.maxsize;
+		else this.sze = Math.max(-1, val);
+		if (this.sizeChanged()) {
+			if (this.buildLines(true)) this.emit(Events.EventDataChanged, this);
 		}
 	}
 
 	get size(): number {
-		return this.sze < 0 ? this.maxData : this.sze;
+		return this.sze > -1
+			? this.maxsize > -1 && this.maxsize < this.sze
+				? this.maxsize
+				: this.sze
+			: this.maxData;
 	}
 
 	/** Get the internal size of the column. (i.e. it returns the actually set value only) */
@@ -48,7 +45,9 @@ export abstract class BaseData extends EventEmitter {
 	 * Build a new array and if the new array is different from the old should return true.
 	 * Else return false.
 	 */
-	abstract buildLines(): boolean;
+	abstract buildLines(force?: boolean): boolean;
+
+	abstract sizeChanged(): boolean;
 
 	get lineCount(): number {
 		return this.lines.length;
