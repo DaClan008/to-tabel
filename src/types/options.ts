95756259b11b7fb424e597edc9cacf726442f85d
Jan Lehmkuhl <DaClan008@users.noreply.github.com>
2020-08-31 00:03:17 +0200

    🧹 - cleaned up the options file slightly

--- src/types/options.ts
+++ src/types/options.ts
@@ -10,88 +10,18 @@ export enum emojiLevel {
 
 export type Options = {
 	/**
-	 * The level of complexity calculating emoji string sizes.
-	 * all: will attempt to convert the most complex emoji string to a single character.
-	 * med: will attempt to calculate the string size based on destructuring and be cognesent of EF0F
-	 * low:  will attempt to calculate the string size based purely on destructuring.
-	 * none: will calculate the actual characters in string (string.length).
-	 * @default all
-	 */
-	eLevel?: emojiLevel;
-	/** Determine if border should be printed. */
-	borders?: borderOptionTypes | boksOptions[] | false;
-	/** Determine the style for the borders if the table has sub tables */
-	subOptions?: (Options | false)[];
-	/**
-	 * a padding to use whithin each column
-	 * @default 2
-	 */
-	padding?: number;
-	/** If set to true, no headers will be printed with the data */
-	excludeHeader?: boolean;
-	/**
-	 * sets the maximum size of the table.
-	 * @default 120
-	 */
-	maxSize?: number;
-
-	/**
-	 * The set size of the table.
-	 * Unlike maxSize, this will force the size to fill if set.
-	 * Will also overrids maxSize if set.
-	 */
-	size?: number;
-	/**
-	 * The spacing from the left corner of the screen.
-	 * @default 2
-	 */
-	margin?: number;
-	/**
-	 * The amount of spaces required for a tab character.
-	 * @default 2
-	 */
-	tabSize?: number;
-	/**
-	 * The depth of the object to print out
-	 * (i.e. if column value is object and depth is 2, a table will be printed inside the column)
-	 * The maximum is 3
-	 * @default 1
-	 */
-	maxDepth?: number;
-
-	/**
 	 * Set the alignment for the content inside the column.
 	 */
 	align?: Alignment;
-
+	/** Determine if and how borders should be printed. */
+	borders?: borderOptionTypes | boksOptions[] | false;
 	/**
-	 * Set the alignment for the header of the column.
-	 * Inherit the value from align if not set.
+	 * used when columns is defined:
+	 * If true, columns not defined in columns option will alsow be added.
+	 * Else, only columns specified in columns option will be added to table.
+	 * Default is false if columns options is used, else true.
 	 */
-	headAlign?: Alignment;
-	/**
-	 * If true and data type is object, the column names will be the object keys.
-	 * Else the columns will be "key" and "value".
-	 * if data is an array of objects the data will always be flatten,
-	 * with each object representing a new row.
-	 * @default false
-	 */
-	flatten?: boolean;
-	/**
-	 * wheather the object should be printed as a continuous stream (if true), or once of table.
-	 * @default false
-	 */
-	stream?: boolean;
-
-	/**
-	 * Store column information and can be:
-	 * STRING ARRAY - containing the names of each column;
-	 * NUMBERS ARRAY - containint the size of each column (size if 1>= and percentage <1);
-	 * columnProperty ARRAY - containing column Property information for each column;
-	 * columnProperties Object.
-	 */
-	columns?: string[] | number[] | columnProperties | columnProperty[];
-
+	canGrow?: boolean;
 	/**
 	 * If set and a column name is only a number (for instance in case of an array object),
 	 * the pattern will be used to generate a display name.
@@ -102,29 +32,91 @@ export type Options = {
 	 */
 	columnPattern?: string | boolean;
 	/**
-	 * used when columns is defined:
-	 * If true, columns not defined in columns option will alsow be added.
-	 * Else, only columns specified in columns option will be added to table.
-	 * Default is false if columns options is used, else true.
+	 * Store column information and can be:
+	 * STRING ARRAY - containing the names of each column;
+	 * NUMBERS ARRAY - containint the size of each column (size if 1>= and percentage <1);
+	 * columnProperty ARRAY - containing column Property information for each column;
+	 * columnProperties Object.
 	 */
-	canGrow?: boolean;
+	columns?: string[] | number[] | columnProperties | columnProperty[];
 	/**
-	 * If true, will automatcially print the object after initialization.
+	 * The level of complexity calculating emoji string sizes.
+	 * all: will attempt to convert the most complex emoji string to a single character.
+	 * med: will attempt to calculate the string size based on destructuring and be cognesent of EF0F
+	 * low:  will attempt to calculate the string size based purely on destructuring.
+	 * none: will calculate the actual characters in string (string.length).
+	 * @default all
 	 */
-	print?: boolean;
-	/**
-	 * If true, all rows including empty rows will be printed,
-	 * else only rows with data will be printed.
-	 * @default false
-	 */
-	inclusive?: boolean;
-
+	eLevel?: emojiLevel;
+	/** If set to true, no headers will be printed with the data */
+	excludeHeader?: boolean;
 	/**
 	 * If true, fills the table to its max or set size.
 	 * else table will only be filled to wrap the actual data size.
 	 * @default false
 	 */
 	fill?: boolean;
+	/**
+	 * If true and data type is object, the column names will be the object keys.
+	 * Else the columns will be "key" and "value".
+	 * if data is an array of objects the data will always be flatten,
+	 * with each object representing a new row.
+	 * @default false
+	 */
+	flatten?: boolean;
+	/**
+	 * Set the alignment for the header of the column.
+	 * Inherit the value from align if not set.
+	 */
+	headAlign?: Alignment;
+	/**
+	 * If true, all rows including empty rows will be printed,
+	 * else only rows with data will be printed.
+	 * @default false
+	 */
+	inclusive?: boolean;
+	/**
+	 * The spacing from the left corner of the screen.
+	 * @default 2
+	 */
+	margin?: number;
+	/**
+	 * The depth of the object to print out
+	 * (i.e. if column value is object and depth is 2, a table will be printed inside the column)
+	 * The maximum is 3
+	 * @default 1
+	 */
+	maxDepth?: number;
+	/**
+	 * sets the maximum size of the table.
+	 * @default 120
+	 */
+	maxSize?: number;
+	/**
+	 * a padding to use whithin each column
+	 * @default 2
+	 */
+	padding?: number;
+	/**
+	 * If true, will automatcially print the object after initialization.
+	 */
+	print?: boolean;
+	/**
+	 * The set size of the table.
+	 * Unlike maxSize, this will force the size to fill if set.
+	 * Will also overrids maxSize if set.
+	 */
+	size?: number;
+	/**
+	 * wheather the object should be printed as a continuous stream (if true), or once of table.
+	 * @default false
+	 */
+	stream?: boolean;
+	/**
+	 * The amount of spaces required for a tab character.
+	 * @default 2
+	 */
+	tabSize?: number;
 };
 
 export type borderOptionTypes = boksOptions | combinedBorders | BorderTypes;
